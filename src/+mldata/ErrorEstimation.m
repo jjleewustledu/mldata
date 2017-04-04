e68f22f2236ed981b3fc2d0fa15618c0b99a3284
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2017-04-04 01:47:18 -0500

    Stub.

---
+++ src/+mldata/ErrorEstimation.m
@@ -0,0 +1,27 @@
+classdef ErrorEstimation 
+	%% ERRORESTIMATION 
+    %  Cf. Bevington & Robinson, Data Reduction and Error Analysis for the
+    %  Physical Sciences, sec. 6.4, ERROR ESTIMATION.
+
+	%  $Revision$
+ 	%  was created 30-Mar-2017 20:51:07 by jjlee,
+ 	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/Local/src/mlcvl/mldata/src/+mldata.
+ 	%% It was developed on Matlab 9.2.0.538062 (R2017a) for MACI64.  Copyright 2017 John Joowon Lee.
+ 	
+	properties
+ 		
+ 	end
+
+	methods 
+		  
+ 		function this = ErrorEstimation(varargin)
+ 			%% ERRORESTIMATION
+ 			%  Usage:  this = ErrorEstimation()
+
+ 			
+ 		end
+ 	end 
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
